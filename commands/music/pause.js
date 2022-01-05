2089db8e95939e19e0c9e86a1b7c2f0f4396d713
Shahinuzzaman Shawn <90779538+8SHAWN8@users.noreply.github.com>
2022-01-05 17:57:05 +0600

    Update pause.js

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -1,28 +1,28 @@
-module.exports = {
+// module.exports = {
 
-        name: 'hi',
-        noalias: 'No Aliases',
-        category: "music",
-        description: 'Pause command.',
-        usage: " ",
-        accessableby: "everyone",
-    run: async (bot, message, args, ops) => {
-        const serverQueue = ops.queue.get(message.guild.id);
-        const { channel } = message.member.voice;
-      try {
-        if (!channel) return message.channel.send('${prefix}I\'m sorry but you need to be in a voice channel to pause music!');
-        // if (message.guild.me.voice.channel !== message.member.voice.channel) {
-        //     return message.channel.send(" You Have To Be In The Same Channel With The Bot! ");
-        // };
-        // if (serverQueue && serverQueue.playing) {
-        //     serverQueue.playing = false;
-        //     serverQueue.connection.dispatcher.pause(true);
-        //     return message.channel.send(' Paused  ⏸');
-        // }
-        return message.channel.send('${prefix} There is Nothing Playing! ');
-      } catch {
-          serverQueue.connection.dispatcher.end();
-          await channel.leave();
-      }
-    }
-};
+//         name: 'hi',
+//         noalias: 'No Aliases',
+//         category: "music",
+//         description: 'Pause command.',
+//         usage: " ",
+//         accessableby: "everyone",
+//     run: async (bot, message, args, ops) => {
+//         const serverQueue = ops.queue.get(message.guild.id);
+//         const { channel } = message.member.voice;
+//       try {
+//         if (!channel) return message.channel.send('${prefix}I\'m sorry but you need to be in a voice channel to pause music!');
+//         // if (message.guild.me.voice.channel !== message.member.voice.channel) {
+//         //     return message.channel.send(" You Have To Be In The Same Channel With The Bot! ");
+//         // };
+//         // if (serverQueue && serverQueue.playing) {
+//         //     serverQueue.playing = false;
+//         //     serverQueue.connection.dispatcher.pause(true);
+//         //     return message.channel.send(' Paused  ⏸');
+//         // }
+//         return message.channel.send('${prefix} There is Nothing Playing! ');
+//       } catch {
+//           serverQueue.connection.dispatcher.end();
+//           await channel.leave();
+//       }
+//     }
+// };
